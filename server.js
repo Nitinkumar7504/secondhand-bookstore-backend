ccdfad0d9d4e7c3576f78836d422c04f72f5442c
Nitin Kumar <nitinkarwasra785@gmail.com>
2026-09-25 17:21:13 +0530

    Configure server for Render

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -6,15 +7,20 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 
+
 app.use(express.json());
 app.use(cookieParser());
+
 
 app.use(cors({
   origin: 'https://secondhand-bookstore-frontend.onrender.com',
   credentials: true,
 }));
 
+
 app.use('/uploads', express.static('uploads'));
+
+
 
 const bookRoutes = require('./routes/books');
 const authRoutes = require('./routes/auth');
@@ -26,18 +32,24 @@ app.use('/api/auth', authRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/admin', adminRoutes);
 
+
 app.get('/', (req, res) => {
   res.send('Secondhand Bookstore API is live!');
 });
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.error('DB Error:', err));
+
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('MongoDB connected');
+  })
+  .catch((err) => {
+    console.error('DB Error:', err);
+  });
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
